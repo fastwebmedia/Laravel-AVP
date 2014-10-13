b759d10979f56f5aaec3fe787edc2466429ae03a
Dan Lawson <daniel.lawson@fastwebmedia.com>
2014-10-13 16:30:00 +0100

    Validation file for after

--- src/lang/en/validation.php
+++ src/lang/en/validation.php
@@ -5,6 +5,7 @@ return array(
 	    'dob.required' => 'Please enter your date of birth',
         'dob.date' => 'Please enter a valid date',
         'dob.date_format' => 'Please enter a valid date in the format yyyy-mm-dd',
+	'dob.after' => 'Please enter a date after 1900',
 	    'dob.before' => 'Sorry, you are not old enough to access this site',
 	    'dob.previous' => 'Sorry, you are not old enough to access this site',
 	),
