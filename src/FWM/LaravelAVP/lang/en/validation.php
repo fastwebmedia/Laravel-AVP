41023c76429097c9efdb0ec94f5cc014049441c9
Dave Carlson <davecarlson@users.noreply.github.com>
2015-03-26 08:43:12 +0000

    Alter too old message

--- src/FWM/LaravelAVP/lang/en/validation.php
+++ src/FWM/LaravelAVP/lang/en/validation.php
@@ -3,9 +3,9 @@
 return array(
 	'custom' => array(
 	    'dob.required' => 'Please enter your date of birth',
-        'dob.date' => 'Please enter a valid date',
-        'dob.date_format' => 'Please enter a valid date in the format yyyy-mm-dd',
-	    'dob.after' => 'Please enter a date after 1900',
+            'dob.date' => 'Please enter a valid date',
+            'dob.date_format' => 'Please enter a valid date in the format yyyy-mm-dd',
+	    'dob.after' => 'You have entered a date too far in the past',
 	    'dob.before' => 'Sorry, you are not old enough to access this site',
 	    'dob.previous' => 'Sorry, you are not old enough to access this site',
 	),
